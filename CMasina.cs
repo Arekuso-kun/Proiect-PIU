8791b02f054ba0ebdc75ff8e9deb0bd7828cd8f3
Arekuso-kun <alexodrino@gmail.com>
2023-03-16 23:20:12 +0200

    Add project files.

---
+++ CMasina.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proiect_PIU
+{
+    class CMasina
+    {
+        private string test;
+
+        public string GetTest()
+        {
+            return test;
+        }
+        public CMasina()
+        {
+            test = "Acesta este un test!";
+        }
+    }
+}
