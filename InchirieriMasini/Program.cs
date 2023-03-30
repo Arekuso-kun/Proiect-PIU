8cb71a4aae0677ebbfed42480563da98f34b77f3
Bîrsan Dorin-Alexandru <83676537+Arekuso-kun@users.noreply.github.com>
2023-03-30 18:13:17 +0300

    Inchiriere variables
    Added dataReturnare, dataPreluare, pret to class Inchiriere

--- InchirieriMasini/Program.cs
+++ InchirieriMasini/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using InchirieriMasiniLibrary;
 using InchirieriMasiniDataManagement;
+using System.Globalization;
 
 namespace InchirieriMasini
 {
@@ -13,18 +14,78 @@ namespace InchirieriMasini
         static void Main(string[] args)
         {
             AdministrareInchirieri fisierTest = new AdministrareInchirieri("data.txt");
-            Inchiriere inchiriere = new Inchiriere(65243, 12312, "Vasile", "Popescu");
+
+            DateTime date1 = new DateTime(2023, 3, 31, 18, 0, 0); // March 31, 2023 6:00:00 PM
+            DateTime date2 = new DateTime(2023, 4, 1, 10, 0, 0); // April 1, 2023 10:00:00 AM
+            Inchiriere inchiriere = new Inchiriere(65243, 12312, "Vasile", "Popescu", 3000.00f, date1, date2);
             fisierTest.AddInchiriere(inchiriere);
             int nrInchirieri = 1;
+
+            bool valid = false;
+
             Console.Write("Introdu nume : ");
             string nume = Console.ReadLine();
             Console.Write("Introdu prenume : ");
             string prenume = Console.ReadLine();
             Console.Write("Introdu ID inchiriere : ");
-            int idInchiriere = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Introdu ID masina : ");
-            int idMasina = Convert.ToInt32(Console.ReadLine());
-            inchiriere = new Inchiriere(idInchiriere, idMasina, nume, prenume);
+            int idInchiriere = int.Parse(Console.ReadLine());
+
+            int idMasina = 0;
+            valid = false;
+            while (!valid)
+            {
+                Console.Write("Introdu ID masina : ");
+                string inputString = Console.ReadLine();
+                valid = int.TryParse(inputString, out idMasina);
+
+                if (!valid)
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                }
+            }
+
+            float pret = 0.0f;
+            valid = false;
+            while (!valid)
+            {
+                Console.Write("Introdu pret : ");
+                string inputString = Console.ReadLine();
+                valid = float.TryParse(inputString, out pret);
+
+                if (!valid)
+                {
+                    Console.WriteLine("Invalid input. Please try again.");
+                }
+            }
+
+            DateTime dataPreluare = DateTime.MinValue;
+            valid = false;
+            string format = "dd/MM/yyyy HH:mm:ss";
+            while (!valid)
+            {
+                Console.Write("Introdu data preluare ( dd/MM/yyyy HH:mm:ss ) : ");
+                string inputString = Console.ReadLine();
+                valid = DateTime.TryParseExact(inputString, format, null, System.Globalization.DateTimeStyles.None, out dataPreluare);
+                if (!valid)
+                {
+                    Console.WriteLine("Invalid format. Please try again.");
+                }
+            }
+
+            DateTime dataReturnare = DateTime.MinValue;
+            valid = false;
+            while (!valid)
+            {
+                Console.Write("Introdu data returnare ( dd/MM/yyyy HH:mm:ss ) : ");
+                string inputString = Console.ReadLine();
+                valid = DateTime.TryParseExact(inputString, format, null, System.Globalization.DateTimeStyles.None, out dataReturnare);
+                if (!valid)
+                {
+                    Console.WriteLine("Invalid format. Please try again.");
+                }
+            }
+
+            inchiriere = new Inchiriere(idInchiriere, idMasina, nume, prenume, pret, dataPreluare, dataReturnare);
             fisierTest.AddInchiriere(inchiriere);
             nrInchirieri++;
             Inchiriere[] inchirieri = fisierTest.GetInchirieri(out nrInchirieri);
@@ -37,10 +98,13 @@ namespace InchirieriMasini
             for (int contor = 0; contor < nrInchirieri; contor++)
             {
                 string infoStudent = string.Format("#{0}: {2} {3} a inchiriat masina #{1}",
-                   inchirieri[contor].GetIDinchiriere(),
-                   inchirieri[contor].GetIDmasina(),
-                   inchirieri[contor].GetNume(),
-                   inchirieri[contor].GetPrenume());
+                   inchirieri[contor].IdInchiriere,
+                   inchirieri[contor].IdMasina,
+                   inchirieri[contor].Nume,
+                   inchirieri[contor].Prenume,
+                   inchirieri[contor].Pret,
+                   inchirieri[contor].DataPreluare,
+                   inchirieri[contor].DataReturnare);
 
             Console.WriteLine(infoStudent);
             }
--- InchirieriMasiniLibrary/Inchiriere.cs
+++ InchirieriMasiniLibrary/Inchiriere.cs
@@ -8,60 +8,95 @@ namespace InchirieriMasiniLibrary
 {
     public class Inchiriere
     {
-        private const char SEPARATOR_PRINCIPAL_FISIER = ',';
+        private const char SEPARATOR_PRINCIPAL_FISIER = ';';
 
         private int idInchiriere;
         private int idMasina;
         private string nume;
         private string prenume;
-        // de adaugat: data inchiriere, durata, pret, ...
+        private float pret;
+        private DateTime dataPreluare;
+        private DateTime dataReturnare;
 
         private const int ID_INCHIRIERE = 0;
         private const int ID_MASINA = 1;
         private const int NUME = 2;
         private const int PRENUME = 3;
-        public Inchiriere(int idInchiriere, int idMasina, string nume, string prenume)
+        private const int PRET = 4;
+        private const int DATA_PRELUARE = 5;
+        private const int DATA_RETURNARE = 6;
+        public Inchiriere(int idInchiriere, int idMasina, string nume, string prenume, float pret, DateTime dataPreluare, DateTime dataReturnare)
         {
-            this.idInchiriere = idInchiriere;
-            this.idMasina = idMasina;
-            this.nume = nume;
-            this.prenume = prenume;
+            IdInchiriere = idInchiriere;
+            IdMasina = idMasina;
+            Nume = nume;
+            Prenume = prenume;
+            Pret = pret;
+            DataPreluare = dataPreluare;
+            DataReturnare = dataReturnare;
         }
         public Inchiriere(string linieFisier)
         {
             var dateFisier = linieFisier.Split(SEPARATOR_PRINCIPAL_FISIER);
 
-            idInchiriere = Convert.ToInt32(dateFisier[ID_INCHIRIERE]);
-            idMasina = Convert.ToInt32(dateFisier[ID_MASINA]);
+            idInchiriere = int.Parse(dateFisier[ID_INCHIRIERE]);
+            idMasina = int.Parse(dateFisier[ID_MASINA]);
             nume = dateFisier[NUME];
             prenume = dateFisier[PRENUME];
+            pret = float.Parse(dateFisier[PRET]);
+            dataPreluare = DateTime.Parse(dateFisier[DATA_PRELUARE]);
+            dataReturnare = DateTime.Parse(dateFisier[DATA_RETURNARE]);
+            // TO DO 
+            // verificari date intrare
         }
         public string ConversieLaSir_PentruFisier()
         {
-            string obiectStudentPentruFisier = string.Format("{1}{0}{2}{0}{3}{0}{4}",
+            string obiectStudentPentruFisier = string.Format("{1}{0}{2}{0}{3}{0}{4}{0}{5}{0}{6}{0}{7}{0}",
                 SEPARATOR_PRINCIPAL_FISIER,
                 idInchiriere.ToString(),
                 idMasina.ToString(),
                 (nume ?? " NECUNOSCUT "),
-                (prenume ?? " NECUNOSCUT "));
+                (prenume ?? " NECUNOSCUT "),
+                (pret.ToString() ?? " NECUNOSCUT "),
+                (dataPreluare.ToString() ?? " NECUNOSCUT "),
+                (dataReturnare.ToString() ?? " NECUNOSCUT "));
 
             return obiectStudentPentruFisier;
         }
-        public int GetIDinchiriere()
+        public int IdInchiriere
         {
-            return idInchiriere;
+            get { return idInchiriere; }
+            set { idInchiriere = value; }
         }
-        public int GetIDmasina()
+        public int IdMasina
         {
-            return idMasina;
+            get { return idMasina; }
+            set { idMasina = value; }
         }
-        public string GetNume()
+        public string Nume
         {
-            return nume;
+            get { return nume; }
+            set { nume = value; }
         }
-        public string GetPrenume()
+        public string Prenume
         {
-            return prenume;
+            get { return prenume; }
+            set { prenume = value; }
+        }
+        public float Pret
+        {
+            get { return pret; }
+            set { pret = value; }
+        }
+        public DateTime DataPreluare
+        {
+            get { return dataPreluare; }
+            set { dataPreluare = value; }
+        }
+        public DateTime DataReturnare
+        {
+            get { return dataReturnare; }
+            set { dataReturnare = value; }
         }
     }
 }
